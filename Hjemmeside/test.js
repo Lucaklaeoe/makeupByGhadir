049e45ae9dee0131e1c1a138a94b21eee6f0133e
Lucaklaeoe <lucaklaeoeskole@gmail.com>
2025-04-22 14:43:03 +0200

    Update test.js

--- Hjemmeside/test.js
+++ Hjemmeside/test.js
@@ -35,12 +35,28 @@ async function testrequest(){
 emailjs.init("8on1XAeHXO55DA6Tp");
 
 document.getElementById('contact-form').addEventListener('submit', function(event) {
-event.preventDefault();
+    event.preventDefault();
 
-emailjs.sendForm('service_kesfnw1', 'template_h23bpoo', this)
-    .then(() => {
-    console.log('SUCCESS!');
-    }, (error) => {
-    console.log('FAILED...', error);
-    });
+    //send email emailjs
+    /*
+    const email_info = {
+        name: "whaaattt",
+        email: "yosajad565@bauscn.com"
+    };
+    emailjs.send('service_kesfnw1', 'template_h23bpoo', email_info)
+        .then(() => {
+        console.log('SUCCESS!');
+        }, (error) => {
+        console.log('FAILED...', error);
+        });
+
+    //send form emailjs 
+    */
+    
+    emailjs.sendForm('service_kesfnw1', 'template_h23bpoo', this)
+        .then(() => {
+        console.log('SUCCESS!');
+        }, (error) => {
+        console.log('FAILED...', error);
+        });
 });
